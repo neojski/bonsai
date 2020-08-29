10b09215276a1e09f6a6dc6ef24fd1f6e84396f8
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2020-08-30 00:40:52 +0100

    wip, slow list

--- web/examples/extensible_list/main.ml
+++ web/examples/extensible_list/main.ml
@@ -1,72 +1,87 @@
 open! Core_kernel
-open Bonsai_web
+open! Bonsai_web
+open! Bonsai.Infix
 
-module Model = struct
+module Row = struct
   type t =
-    { data : unit Int.Map.t
-    ; count : int
+    { id: int
+    ; value : string
     }
-  [@@deriving fields, equal, sexp]
+  [@@deriving equal, sexp]
 
-  let default = { data = Int.Map.empty; count = 0 }
+  let test = { id = 0; value = "test" }
+
+  let _x = test
 end
 
-module Action = struct
-  type t =
-    | Add
-    | Remove of int
-  [@@deriving sexp_of]
+module Row_component = struct
+  module Input = Row
+  module Result = Vdom.Node
+  module Model = Unit
+
+  module Action = struct
+    type t [@@deriving sexp]
+  end
+
+  let apply_action ~inject:_ ~schedule_event:_ _ _ = assert false
+
+  let compute ~inject:_ { Row.id; value } () =
+    Vdom.Node.div
+      []
+      [ Vdom.Node.textf "(elaborate) key: %d; value: %s" id value ]
+
+  let name = Source_code_position.to_string [%here]
 end
 
-let state_component =
-  Bonsai.state_machine
-    (module Model)
-    (module Action)
-    [%here]
-    ~default_model:Model.default
-    ~apply_action:(fun ~inject:_ ~schedule_event:_ _ model -> function
-      | Action.Add ->
-        let key = model.Model.count in
-        { data = Map.add_exn model.data ~key ~data:(); count = key + 1 }
-      | Action.Remove key -> { model with data = Map.remove model.Model.data key })
-;;
+let _row = Bonsai.of_module ~default_model:() (module Row_component)
 
-let add_remove component ~wrap_remove =
-  let open Bonsai.Arrow in
-  let open Bonsai.Let_syntax in
-  let associated = Bonsai.Map.assoc_input (module Int) component in
-  let%map results, inject_action =
-    state_component >>> first (Model.data @>> associated)
-  in
-  let data =
-    results
-    |> Map.mapi ~f:(fun ~key ~data ->
-      let inject_remove = inject_action (Action.Remove key) in
-      wrap_remove data inject_remove)
-    |> Map.data
-  in
-  data, inject_action Action.Add
-;;
-
-let add_remove_vdom component =
-  let open Vdom in
-  let%map.Bonsai views, add_event =
-    add_remove component ~wrap_remove:(fun view remove_event ->
-      Node.div
+let _row2 =
+  Bonsai.pure ~f:(fun { Row.id; value } ->
+      Vdom.Node.div
         []
-        [ Node.button [ Attr.on_click (fun _ -> remove_event) ] [ Node.text "X" ]
-        ; view
+        [ Vdom.Node.textf "(pure) key: %d; value: %s" id value ])
+
+let row3 =
+  Bonsai.With_incr.pure ~f:(fun incr ->
+      let%map.Incr { Row.id; value } = incr in
+      Vdom.Node.div
+        []
+        [ Vdom.Node.textf "(pure) key: %d" id
+        ; Vdom.Node.textf "(pure) value: %s" value
         ])
-  in
-  Node.div
-    []
-    (Node.button [ Attr.on_click (fun _ -> add_event) ] [ Node.text "Add" ] :: views)
-;;
+
+let map component =
+  let associated = Bonsai.Map.assoc_input (module Int) component in
+  let%map.Bonsai y = associated in
+  let z =  Map.data y in
+  Vdom.Node.div [] z
 
 let (_ : _ Start.Handle.t) =
-  Start.start_standalone
-    (add_remove_vdom Bonsai_web_counters_example.single_counter)
-    ~initial_input:()
-    ~bind_to_element_with_id:"app"
-;;
+  let incr =
+    let init = 
+      List.init 10000 ~f:(fun id ->
+          id, { Row.id; value = Int.to_string id })
+      |> Int.Map.of_alist_exn
+    in
+    let var = Incr.Var.create init in
 
+    (
+      let i = ref 0 in
+      let open Async_kernel in
+      Clock_ns.every (Time_ns.Span.of_int_ms 10) (fun () ->
+          incr i;
+          let key = !i in
+          Incr.Var.replace var ~f:(fun map ->
+              Map.update map key ~f:(fun row ->
+                  match row with
+                  | Some { Row.id; value } -> { Row.id; value = value ^ "x" }
+                  | None -> { Row.id = key; value = "new" }
+            ))
+      )
+    );
+
+    Incr.Var.watch var
+  in
+  let component = Bonsai.With_incr.of_incr incr >>> map row3 in
+  Start.start_standalone ~initial_input:incr ~bind_to_element_with_id:"app" component
+;;
